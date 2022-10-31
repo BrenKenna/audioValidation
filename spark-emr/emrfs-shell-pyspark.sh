a9f13dc0181040d47ba5f83b038e0a2921216d63
Brendan Kenna <brendan.kenna.bk@gmail.com>
2022-10-31 15:55:48 +0000

    Update emrfs-shell-pyspark.sh

--- spark-emr/emrfs-shell-pyspark.sh
+++ spark-emr/emrfs-shell-pyspark.sh
@@ -51,3 +51,27 @@ decompr[0:3]
    '22/10/28 10:53:46 INFO SignalUtils: Registering signal handler for INT'
 ]
 """
+
+
+
+#
+import boto3
+s3 = boto3.client('s3')
+bucket = "bk-spark-cluster-tf"
+prefix = "spark/j-DHIV6JGGHIKY/containers/application_1666953345948_0001/container_1666953345948_0001_01_000001/"
+response = s3.list_objects(Bucket = bucket, Prefix = prefix)
+response.keys()
+response['Contents'][0].keys()
+
+
+"""
+
+
+- Response object
+dict_keys(['ResponseMetadata', 'IsTruncated', 'Marker', 'Contents', 'Name', 'Prefix', 'MaxKeys', 'EncodingType'])
+
+
+- Content objects
+dict_keys(['Key', 'LastModified', 'ETag', 'Size', 'StorageClass', 'Owner'])
+
+"""
